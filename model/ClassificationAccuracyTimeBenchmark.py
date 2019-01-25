a612d7279bea61c3b577741d5c4f631ad3d06dd6
pete meltzer <meltzerpete@gmail.com>
2019-01-25 01:15:04 +0000

    results then refactor

--- model/ClassificationAccuracyTimeBenchmark.py
+++ model/ClassificationAccuracyTimeBenchmark.py
@@ -1,7 +1,7 @@
 from keras.models import Model
 from math import ceil
 from keras.utils import to_categorical
-from ImportData import DropboxLoader as dl
+from ImportData import DropboxLoader
 import networkx as nx
 import pandas as pd
 import numpy as np
@@ -25,71 +25,119 @@ def preprocess(A):
     return A_
 
 
+def get_A_X(loader):
+    all_adj = loader.get_adj()
+    graph_ind = loader.get_graph_ind()
+    numGraphs = graph_ind.max()['graph_ind']
+
+    def get_A_X_by_graph_id(id):
+        nodes_to_keep = graph_ind.loc[graph_ind['graph_ind'] == id]['node']
+        adj = all_adj[all_adj['from'].isin(nodes_to_keep) | all_adj['to'].isin(nodes_to_keep)]
+
+        all_X = loader.get_node_label()
+        ids = all_X[all_X['node'].isin(nodes_to_keep)].index
+        X = pd.get_dummies(all_X['label']).iloc[ids].values
+        G = nx.from_pandas_edgelist(adj, 'from', 'to')
+        A = nx.to_numpy_array(G)
+        return A, X
+
+    A, X = zip(*[get_A_X_by_graph_id(id) for id in range(1, numGraphs + 1)])
+    num_nodes_list = list(map(lambda a: a.shape[0], X))
+    maxNodes = max(num_nodes_list)
+
+    def normaliseX_by_num_nodes(x_num_nodes):
+        x, num_nodes = x_num_nodes
+        return np.divide(x, num_nodes)
+
+    def padA(a):
+        padA = np.zeros([maxNodes, maxNodes])
+        padA[:a.shape[0], :a.shape[1]] = a
+        return padA
+
+    def padX(x):
+        padX = np.zeros([maxNodes, x.shape[1]])
+        padX[:x.shape[0], :x.shape[1]] = x
+        return padX
+
+    padA = list(map(padA, A))
+    padX = list(map(padX, map(normaliseX_by_num_nodes, zip(X, num_nodes_list))))
+
+    return padA, padX
+
+
+def get_data(dropbox_name, file_name_ext):
+    dataset = DropboxLoader(dropbox_name)
+
+    if os.path.isfile(dropbox_name + file_name_ext):
+        A_list, X = pickle.load(open(dropbox_name + file_name_ext, "rb"))
+    else:
+        A, X = get_A_X(dataset)
+        A_list = list(map(csr_matrix, map(preprocess, A)))
+        pickle.dump((A_list, X), open(dropbox_name + file_name_ext, "wb"))
+    Y = dataset.get_graph_label()
+
+    return A_list, X, Y
+
+
 with open('out.csv', 'a') as csv_file:
     res_writer = writer(csv_file, delimiter=';')
     res_writer.writerow(
         ["dataset", "batch_size", "mean_acc", "acc_std", "mean_train_time(s)", "time_std", "all_accs", "all_times"])
 
+    datasets = {
+        'ENZYMES': {
+            'preprocess_graph_labels': lambda x: x - 1,
+            'classes': 6,
+        },
+        'MUTAG': {
 
-    def get_A_X(loader):
-        all_adj = loader.get_adj()
-        graph_ind = loader.get_graph_ind()
-        numGraphs = graph_ind.max()['graph_ind']
+        },
+        'NCI1': {
+            'pretty_name': 'NCI-1',
+        },
+        'NCI109': {
+            'pretty_name': 'NCI-109',
+        },
+        'PTC_MM': {
+            'pretty_name': 'PTC-MM',
+        },
+        'PTC_FM': {
+            'pretty_name': 'PTC-FM',
+        },
+        'PTC_MR': {
+            'pretty_name': 'PTC-MR',
+        },
+        'PTC_FR': {
+            'pretty_name': 'PTC-FR',
+        },
+    }
 
-        def get_A_X_by_graph_id(id):
-            nodes_to_keep = graph_ind.loc[graph_ind['graph_ind'] == id]['node']
-            adj = all_adj[all_adj['from'].isin(nodes_to_keep) | all_adj['to'].isin(nodes_to_keep)]
+    batch_sizes = [50]
+    file_name_ext = "_normalise.p"
 
-            all_X = loader.get_node_label()
-            ids = all_X[all_X['node'].isin(nodes_to_keep)].index
-            X = pd.get_dummies(all_X['label']).iloc[ids].values
-            G = nx.from_pandas_edgelist(adj, 'from', 'to')
-            A = nx.to_numpy_array(G)
-            return A, X
+    out_dim_a2 = 64
+    out_dim_x2 = 64
 
-        A, X = zip(*[get_A_X_by_graph_id(id) for id in range(1, numGraphs + 1)])
-        num_nodes_list = list(map(lambda a: a.shape[0], A))
-        maxNodes = max(num_nodes_list)
+    for dataset_name, dataset in datasets.items():
 
-        def normaliseX_by_num_nodes(x_num_nodes):
-            x, num_nodes = x_num_nodes
-            return np.divide(x, num_nodes)
+        print("Dataset: ", dataset_name)
 
-        def padA(a):
-            padA = np.zeros([maxNodes, maxNodes])
-            padA[:a.shape[0], :a.shape[1]] = a
-            return padA
+        dropbox_name = dataset['dropbox_name'] \
+            if 'dropbox_name' in dataset.keys() else dataset_name
 
-        def padX(x):
-            padX = np.zeros([maxNodes, x.shape[1]])
-            padX[:x.shape[0], :x.shape[1]] = x
-            return padX
+        preprocess_graph_labels = dataset['preprocess_graph_labels'] \
+            if 'preprocess_graph_labels' in dataset.keys() else lambda x: 1 if x == 1 else 0
 
-        padA = list(map(padA, A))
-        padX = list(map(padX, map(normaliseX_by_num_nodes, zip(X, num_nodes_list))))
+        classes = dataset['classes'] \
+            if 'classes' in dataset.keys() else 2
 
-        return padA, padX
-
-
-    for dataset_name in ['MUTAG']:
-    # for dataset_name in ['NCI1', 'NCI109']:
-    # for dataset_name in ['PTC_MM', 'PTC_FM', 'PTC_MR', 'PTC_FR']:
-        for batch_size in [50]:
+        for batch_size in batch_sizes:
             # prepare data
             print("preparing data")
-            dataset = dl.DropboxLoader(dataset_name)
 
-            file_name_ext = "_normalise.p"
-            if os.path.isfile(dataset_name + file_name_ext):
-                A_list, X = pickle.load(open(dataset_name + file_name_ext, "rb"))
-            else:
-                A, X = get_A_X(dataset)
-                A_list = list(map(csr_matrix, map(preprocess, A)))
-                pickle.dump((A_list, X), open(dataset_name + file_name_ext, "wb"))
+            A_list, X, Y = get_data(dropbox_name, file_name_ext)
 
-            Y = dataset.get_graph_label()
-
-            Y['graph_label'] = Y['graph_label'].apply(lambda x: 1 if x == 1 else 0)
+            Y['graph_label'] = Y['graph_label'].apply(preprocess_graph_labels)
             Y = np.array(Y)
 
             folds = list(StratifiedKFold(n_splits=10, shuffle=True).split(X, Y))
@@ -101,40 +149,32 @@ with open('out.csv', 'a') as csv_file:
 
                 A_train = np.array([A_list[i] for i in train_idx])
                 X_train = np.array([X[i] for i in train_idx])
-                # X_train = X[train_idx]
                 Y_train = to_categorical(Y)[train_idx]
 
                 A_test = np.array([A_list[i] for i in val_idx])
                 X_test = np.array([X[i] for i in val_idx])
-                # X_test = X[val_idx]
                 Y_test = to_categorical(Y)[val_idx]
 
                 A_in = Input((X[0].shape[0], X[0].shape[0]), name='A_in')
                 X_in = Input(X[0].shape, name='X_in')
 
-                # x1 = Dropout(0.5)(inputs)
                 x1 = MyGCN(100, activation='relu')([A_in, X_in])
-                # x1 = Dropout(0.5)(x1)
-                x1 = MyGCN(64, activation='softmax')([A_in, x1])
-                # x1 = Dropout(0.5)(x1)
+                x1 = MyGCN(out_dim_a2, activation='softmax')([A_in, x1])
 
-                # x2 = Dropout(0.5)(inputs)
                 x2 = MyGCN(100, activation='relu')([A_in, X_in])
-                # x2 = Dropout(0.5)(x2)
-                x2 = MyGCN(64, activation='relu')([A_in, x2])
-                # x2 = Dropout(0.5)(x2)
+                x2 = MyGCN(out_dim_x2, activation='relu')([A_in, x2])
 
                 x3 = Dot(axes=[1, 1])([x1, x2])
-                x3 = Reshape((64**2,))(x3)
+                x3 = Reshape((out_dim_a2 * out_dim_x2,))(x3)
 
-                x4 = Dense(2, activation='softmax')(x3)
+                x4 = Dense(classes, activation='softmax')(x3)
 
                 model = Model(inputs=[A_in, X_in], outputs=x4)
                 model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
 
-                # model.summary()
+                model.summary()
 
-                tb_callback = keras.callbacks.TensorBoard(log_dir='./Graph/' + dataset_name + '/' + str(j),
+                tb_callback = keras.callbacks.TensorBoard(log_dir='./Graph/' + dropbox_name + '/' + str(j),
                                                           histogram_freq=0,
                                                           write_grads=False,
                                                           write_graph=True, write_images=False)
@@ -189,7 +229,7 @@ with open('out.csv', 'a') as csv_file:
 
                 accuracies.append(stats[1])
 
-            print(dataset_name)
+            print(dropbox_name)
             mean_acc = np.mean(accuracies)
             print("mean acc:", mean_acc)
             acc_std = np.std(accuracies)
@@ -200,5 +240,5 @@ with open('out.csv', 'a') as csv_file:
             time_std = np.std(times)
             print("std dev:", time_std, end="\n\n")
 
-            res_writer.writerow([dataset_name, batch_size, mean_acc, acc_std, mean_time, time_std, accuracies, times])
+            res_writer.writerow([dropbox_name, batch_size, mean_acc, acc_std, mean_time, time_std, accuracies, times])
             csv_file.flush()
