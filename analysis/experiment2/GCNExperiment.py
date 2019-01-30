e629ebb280602a1bc30f6a0daf292ea971b77105
pete meltzer <meltzerpete@gmail.com>
2019-01-30 14:05:07 +0000

    added sum

--- analysis/experiment2/GCNExperiment.py
+++ analysis/experiment2/GCNExperiment.py
@@ -2,10 +2,11 @@ from math import ceil
 
 import numpy as np
 from keras import Model
-from keras.layers import Dense, Flatten
+from keras.layers import Dense, Flatten, Lambda
 from keras.models import Input
 from keras.optimizers import Adam
 from sklearn.model_selection import StratifiedKFold, train_test_split, StratifiedShuffleSplit
+from keras import backend as K
 
 from analysis.experiment2 import generate
 from model.ClassificationAccuracyTimeBenchmark import split_test_train
@@ -39,19 +40,19 @@ for train_idx, val_idx in iter(splits):
 
     x1 = MyGCN(100, activation='relu')([A_in, X_in])
     x2 = MyGCN(64, activation='relu')([A_in, x1])
-    x3 = Flatten()(x2)
+    x3 = Lambda(lambda x: K.sum(x, axis=1))(x2)
     x4 = Dense(num_graph_classes, activation='softmax')(x3)
 
     model = Model(inputs=[A_in, X_in], outputs=x4)
 
-    # print(model.summary())
+    print(model.summary())
 
     model.compile(Adam(), loss='categorical_crossentropy', metrics=['acc'])
     generator = GraphClassifier().batch_generator([A_train, X_train], y_train, batch_size)
     model.fit_generator(generator, ceil(y_train.shape[0] / batch_size), 200, verbose=0)
 
     stats = model.evaluate_generator(
-        GraphClassifier().batch_generator([A_test, X_test], y_test, 5), y_test.shape[0] / batch_size)
+        GraphClassifier().batch_generator([A_test, X_test], y_test, batch_size), y_test.shape[0] / batch_size)
 
     for metric, val in zip(model.metrics_names, stats):
         print(metric + ": ", val)
